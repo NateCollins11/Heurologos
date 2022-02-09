7bd724c8ae46d78fb36045a0be9ee21cbc640486
Nathaniel Collins <nathanielcollins@nathaniels-mbp.lan>
2022-02-08 22:49:39 -0500

    adding 6-letter wordle

--- js/wordle.js
+++ js/wordle.js
@@ -1,11 +1,32 @@
 // const { listen } = require("express/lib/application");
 
+
+
 function WordleSetUp(wordLength){
 
     wordleRunning = true;
 
-    var word = 'farts';
+    var possibleAnswers;
 
+    if (wordLength == 5){
+
+        possibleAnswers = answers_5
+
+    }
+    if (wordLength == 6){
+
+        possibleAnswers = answers_6
+
+    }
+
+    var randomWordIndex = Math.floor(Math.random() * possibleAnswers.length)
+
+    var word = possibleAnswers[randomWordIndex];
+
+
+
+
+    var gameSize = word.length;
     
     var currentLine = 0;
 
@@ -22,7 +43,7 @@ function WordleSetUp(wordLength){
         // console.log(e.key);
 
         if ('abcdefghijklmnopqrstuvwxyz'.includes(e.key)){
-            if (currentGuess.length < 5){
+            if (currentGuess.length < gameSize){
                 currentGuess = currentGuess + e.key;
             
                 // console.log(currentGuess);
@@ -35,7 +56,7 @@ function WordleSetUp(wordLength){
 
         if (e.key == 'Enter'){
             
-            if (currentGuess.length == 5 && IsWord(currentGuess)){
+            if (currentGuess.length == gameSize && IsWord(currentGuess)){
 
             
             HandleSubmitGuess(currentGuess)
@@ -65,12 +86,12 @@ function drawWordle(currentLine, currentGuess, previousGuesses){
     for (let y = 0; y < 7; y ++){
         c.beginPath()
         c.moveTo(250, 70 * y+ 50);
-        c.lineTo(650, 70 * y+ 50);
+        c.lineTo(730, 70 * y+ 50);
         c.stroke();
         
     }
 
-    for (let x = 0; x < 6; x ++){
+    for (let x = 0; x < gameSize + 1; x ++){
         c.beginPath()
         c.moveTo(250 + x * 80, 50);
         c.lineTo(250 + x * 80, 470);
@@ -103,6 +124,12 @@ function drawWordle(currentLine, currentGuess, previousGuesses){
                 c.fillStyle='#888800';
                 c.fillRect(250 + index * 80, 50 + lineNumber * 70, 80, 70);
             }
+            else{
+            
+                c.fillStyle='#aaaaaa';
+                c.fillRect(250 + index * 80, 50 + lineNumber * 70, 80, 70);
+            }
+        
 
             c.fillStyle = '#000000';
 
